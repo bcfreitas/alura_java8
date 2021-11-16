381ae86880491fe6197fb11f49a3aa3292e6ea78
Bruno Freitas <bcfreitas@gmail.com>
2021-11-16 20:30:14 -0300

    chegada de Lambda Expressions

--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -15,7 +15,26 @@ public class OrdenaString {
 		Comparator<String> comparador = new ComparadorPorTamanho();
 		//Collections.sort(palavras, comparador);
 		//agora a própria interface List possui o sort:
-		palavras.sort(comparador);
+		
+		//em lugar de declarar uma classe que implementa a interface Comparator,
+		//como ela é uma interface funcional, podemos usar lambda expressions:
+		palavras.sort((s1, s2) -> {
+		    if (s1.length() < s2.length())
+		        return -1;
+		    if (s1.length() > s2.length())
+		        return 1;
+		    return 0;
+		});
+		
+		//também podemos simplificar usando o compare do Integer:
+		palavras.sort((s1, s2) -> {
+		    return Integer.compare(s1.length(), s2.length());
+		});
+		
+		//e simplificar ainda mais:
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		
 		//se olharmos o source deste método, veremos
 		//que foi usado o modificador default, que permite a evolução 
 		//da interface sem quebrar código legado (exigindo implementação)
@@ -29,17 +48,49 @@ public class OrdenaString {
 		}
 		
 		//Nova forma: forEach (método da interface Iterable) com consumer
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		//Normalmente em lugar de criar uma classe interna e chamá-la se utiliza 
+		//classe anônima
+		//Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(new Consumer<String>() {
+		    public void accept(String s) {
+		        System.out.println(s);
+		    }
+		});
+		
+		//mas com o java 8 surgiu a expressão lambda.
+		//o lambda se encaixa em contextos para substituir implementações 
+		//de interfaces funcionais, que são as que possuem um único 
+		//método abstrato (método sem corpo, e métodos sobrescritos 
+		//de Object não contam).
+		palavras.forEach((String s) -> {
+		    System.out.println(s);
+		});
+		
+		//além de nem precisar falar do método accept para este caso, também
+		//podemos sumrimir a declaração do tipo do parâmetro, pois é inferido. 
+		palavras.forEach((s) -> {
+		    System.out.println(s);
+		});
+		
+		//e quando há apenas um parâmetro, até os parênteses podem ser suprimidos.
+		palavras.forEach(s -> {
+		    System.out.println(s);
+		});
+		
+		//e pra simplificar, tiramos até as chaves:
+		palavras.forEach(s -> System.out.println(s));
 	}
 }
 
-class ImprimeNaLinha implements Consumer<String>{
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-}
+
+//A classe interna ficou desnecessária pois em seu lugar foi
+//usada classe anônima
+//class ImprimeNaLinha implements Consumer<String>{
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);
+//	}
+//}
 
 class ComparadorPorTamanho implements Comparator<String> {
 	@Override
