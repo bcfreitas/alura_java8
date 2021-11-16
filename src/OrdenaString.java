ae1ecc78687072cd07f1a7524dc89c62e97a6c00
Bruno Freitas <bcfreitas@gmail.com>
2021-11-15 23:22:38 -0300

    primeiro commit: old way java para ordenar coleção

---
+++ src/OrdenaString.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaString {
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<String>();
+		
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+
+		//old way para comparar/ordenar até o Java 7
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		Collections.sort(palavras, comparador);
+		System.out.println(palavras);
+	}
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length() < s2.length())
+			return -1;
+		if(s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+}
