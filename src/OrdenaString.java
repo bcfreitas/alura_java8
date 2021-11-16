981f8ffe5f920ae8783708290dd3c39523dc65fc
Bruno Freitas <bcfreitas@gmail.com>
2021-11-15 23:33:37 -0300

    1. sort diretamente na lista; 2. forEach com consumer para iterar

--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaString {
 	public static void main(String[] args) {
@@ -13,8 +13,31 @@ public class OrdenaString {
 
 		//old way para comparar/ordenar até o Java 7
 		Comparator<String> comparador = new ComparadorPorTamanho();
-		Collections.sort(palavras, comparador);
+		//Collections.sort(palavras, comparador);
+		//agora a própria interface List possui o sort:
+		palavras.sort(comparador);
+		//se olharmos o source deste método, veremos
+		//que foi usado o modificador default, que permite a evolução 
+		//da interface sem quebrar código legado (exigindo implementação)
+		//outra novidade é que há implementação no método da interface, o que
+		//não era possível até o java 7.
 		System.out.println(palavras);
+		
+		//old way enhanced for
+		for(String p: palavras) {
+			System.out.println(p);
+		}
+		
+		//Nova forma: forEach (método da interface Iterable) com consumer
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+	}
+}
+
+class ImprimeNaLinha implements Consumer<String>{
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
 	}
 }
 
