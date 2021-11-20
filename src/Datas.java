5d6ffd3bcb7c5c363630904550399687b6a59b43
Bruno Freitas <bcfreitas@gmail.com>
2021-11-19 22:38:26 -0300

    Date and Time

---
+++ src/Datas.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Datas {
+	public static void main(String[] args) {
+		LocalDate hoje = LocalDate.now();
+		System.out.println(hoje);
+		
+		//LocalDate é o novo objeto para datas no Java 8
+		LocalDate olimpiadasRio = LocalDate.of(2016,  Month.JUNE, 5);
+		
+		int anos = olimpiadasRio.getYear() - hoje.getYear();
+		
+		System.out.println(anos);
+		
+		Period periodo = Period.between(hoje, olimpiadasRio);
+		System.out.println(periodo.getYears());
+		
+		LocalDate proximasOlimpiadas = olimpiadasRio.plusYears(4);
+		System.out.println(proximasOlimpiadas);
+		
+		//Para formatar ficou bem mais simples!
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String valorFormatado = proximasOlimpiadas.format(formatador);
+		System.out.println(valorFormatado);
+		
+		//Para data com hora, usar LocalDateTime
+		DateTimeFormatter formatadorComHoras = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+		
+		LocalDateTime agora = LocalDateTime.now();
+		System.out.println(agora.format(formatadorComHoras));
+		
+		LocalTime intervalo = LocalTime.of(15, 30);
+		
+		System.out.println(intervalo);
+	}
+}
